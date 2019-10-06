df3ea7a2645dcbac2178a7c6f91bfc0b1f23d16a
mayudong <mayudong200333@gmail.com>
2019-10-06 17:42:24 +0900

    Initial Commit

---
+++ FirstKadaiAppTests/FirstKadaiAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  FirstKadaiAppTests.swift
+//  FirstKadaiAppTests
+//
+//  Created by 馬煜東 on 2019/10/06.
+//  Copyright © 2019年 ikutou.ba. All rights reserved.
+//
+
+import XCTest
+@testable import FirstKadaiApp
+
+class FirstKadaiAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
